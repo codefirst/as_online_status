c77634a8cb42c2804fd102613f1346ae87c78ec2
SHIMOMURA Sho <graueule@gmail.com>
2013-04-05 21:16:13 +0900

    move dom manipulation codes to static css files

--- app/assets/javascripts/online_status.js
+++ app/assets/javascripts/online_status.js
@@ -20,35 +20,32 @@
     function checkOnlineUsers(){
         var list_url = location.origin + "/api/v1/online_status/list";
         jQuery.get(list_url, {room_id : AsakusaSatellite.current.room}, function(users){
+            var user_ids = $.map(users, function(user){return user._id});
+            $.each(online_user_ids, function(idx, id){
+                if ($.inArray(id, user_ids) == -1) {
+                    $("#online_status_user_"+id).remove();
+                    online_user_ids = online_user_ids.filter(function(_id){return id != _id});
+                }
+            });
+
             $.each(users, function(idx, user){
                 if ($.inArray(user._id, online_user_ids) == -1) {
                     var card = createCard(user);
                     onlinestatus_section.append(card);
                     online_user_ids.push(user._id);
                 }
-            })
+            });
         })
     }
 
     function createCard(user){
-        var card = $("<div></div>").css({
-            "margin" : "5px",
-            "padding" : "5px",
-            "min-height" : "40px",
-            "border-bottom" : "1px solid rgba(0,0,0,0.2)",
-            "-webkit-box-shadow" : "0px 1px 0px rgba(255,255,255,0.3)",
-        });
-        var img = $("<img src='"+user.profile_image_url+"'></img>").css({
-            "-webkit-border-radius" : "5px",
-            "width" : "40px",
-            "height" : "40px",
-            "float" : "left",
-        });
-        var info = $("<div></div>").css({
-            "margin-left" : "45px",
-        });
-        var username = $("<span>"+user.screen_name+"</span>").css("display", "block");
-        var status = $("<span>"+user.name+"</span>");
+        var card = $("<div></div>").
+            attr("id","online_status_user_"+user._id).
+            addClass("online-user");
+        var img = $("<img></img>").attr("src", user.profile_image_url);
+        var info = $("<div></div>").addClass("info");
+        var username = $("<span></span>").text(user.screen_name).css("display", "block");
+        var status = $("<span></span>").text(user.name);
         info.append(username).append(status);
         card.append(img).append(info);
 
